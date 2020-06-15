5751cb19875d986325aef233aa306d1c7271748c
rahulshuvo <rahulshuvo5@gmail.com>
2020-06-15 08:27:58 +0600

    finished the drag and drop application, worked with card, list

--- src/components/TrelloCard.js
+++ src/components/TrelloCard.js
@@ -3,28 +3,29 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  margin-bottom: 8px;
+`;
 
 export default function TrelloCard({ text, id, index }) {
   return (
     <Draggable draggableId={String(id)} index={index}>
       {(provided) => (
-        <div
+        <CardContainer
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          <Card style={styles.cardContainer}>
+          <Card>
             <CardContent>
               <Typography gutterBottom>{text}</Typography>
             </CardContent>
           </Card>
-        </div>
+        </CardContainer>
       )}
     </Draggable>
   );
 }
-const styles = {
-  cardContainer: {
-    marginBottom: 8,
-  },
-};
+
--- src/reducers/listsReducer.js
+++ src/reducers/listsReducer.js
@@ -1,70 +1,133 @@
-import {CONSTANTS} from '../actions';
-import {v4 as uuid} from 'uuid';
+import { CONSTANTS } from "../actions";
+import { v4 as uuid } from "uuid";
 
+const initialState = [
+  {
+    id: uuid(),
+    title: "list title",
+    cards: [
+      {
+        id: uuid(),
+        text: "first card",
+      },
+      {
+        id: uuid(),
+        text: "second card",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "list title 2",
+    cards: [
+      {
+        id: uuid(),
+        text: "first card",
+      },
+      {
+        id: uuid(),
+        text: "second card",
+      },
+      {
+        id: uuid(),
+        text: "third card",
+      },
+      {
+        id: uuid(),
+        text: "first card",
+      },
+      {
+        id: uuid(),
+        text: "second card",
+      },
+      {
+        id: uuid(),
+        text: "third card",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "list title",
+    cards: [
+      {
+        id: uuid(),
+        text: "first card",
+      },
+      {
+        id: uuid(),
+        text: "second card",
+      },
+      {
+        id: uuid(),
+        text: "third card",
+      },
+      {
+        id: uuid(),
+        text: "fourth card",
+      },
+    ],
+  },
+];
 
-const initialState=[
-    {
-        id:uuid(),
-        title:'list title',
-        cards:[
-            {
-                id:uuid(),
-                text:'first card'
-            },
-            {
-                id:uuid(),
-                text:'second card'
-            },
-        ]
-    },
-    {
-        id:uuid(),
-        title:'list title 2',
-        cards:[
-            {
-                id:uuid(),
-                text:'first card'
-            },
-            {
-                id:uuid(),
-                text:'second card'
-            },
-            {
-                id:uuid(),
-                text:'third card'
-            },
-        ]
-    },
-]
+const listsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CONSTANTS.ADD_LIST:
+      const newList = {
+        title: action.payload.title,
+        id: uuid(),
+        cards: [],
+      };
+      return [...state, newList];
+    case CONSTANTS.ADD_CARD:
+      const newCard = {
+        text: action.payload.text,
+        id: uuid(),
+      };
 
-const listsReducer= (state=initialState, action) =>{
-    switch(action.type){
-        case CONSTANTS.ADD_LIST:
-            const newList={
-                title: action.payload.title,
-                id:uuid(),
-                cards:[]
-            }
-            return [...state, newList];
-        case CONSTANTS.ADD_CARD:
-            const newCard={
-                text:action.payload.text,
-                id:uuid(),
-            }
+      const newState = state.map((list) => {
+        if (list.id === action.payload.listID) {
+          return { ...list, cards: [...list.cards, newCard] };
+        } else return list;
+      });
+      return newState;
+    case CONSTANTS.DRAG_HAPPEND: {
+      const {
+        droppableIdStart,
+        droppableIdEnd,
+        droppableIndexStart,
+        droppableIndexEnd,
+        draggableId,
+        type,
+      } = action.payload;
+      const newState = [...state];
+      //dragging list
+      if(type==='list')
+      {
+        const list = newState.splice(droppableIndexStart,1);
+        newState.splice(droppableIndexEnd,0, ...list);
+        return newState;
+      }
+      //in the same list
+      if (droppableIdStart === droppableIdEnd) {
+        const list = state.find((list) => droppableIdStart === list.id);
+        const card = list.cards.splice(droppableIndexStart, 1);
+        list.cards.splice(droppableIndexEnd, 0, ...card);
+      }
+      if (droppableIdStart !== droppableIdEnd) {
+        const liststart = state.find((list) => droppableIdStart === list.id);
+        const card = liststart.cards.splice(droppableIndexStart, 1);
 
-            const newState=state.map((list)=>{
-                if(list.id===action.payload.listID){
-                    return{...list,
-                        cards:[...list.cards,newCard]
+        const listEnd = state.find((list) => droppableIdEnd === list.id);
+        listEnd.cards.splice(droppableIndexEnd, 0, ...card);
+      }
 
-                    }
-                }
-                else return list;
-            })
-            return newState;
-        default:
-            return state;
+      return newState;
     }
-}
+
+    default:
+      return state;
+  }
+};
 
 export default listsReducer;
